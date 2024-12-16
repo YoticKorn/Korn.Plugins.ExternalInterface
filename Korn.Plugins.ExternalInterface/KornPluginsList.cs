7b8e7ab276c1d950230f6c198e6a99ca0a02e2b0
Yotic <yoticcitoy@gmail.com>
2024-12-16 04:25:40 +0300

    Add project files.

---
+++ Korn.Plugins.ExternalInterface/KornPluginsList.cs
@@ -0,0 +1,21 @@
+﻿using Korn.Utils;
+
+namespace Korn.Plugins.ExternalInterface;
+public class KornPluginsList
+{
+    static readonly string KornPath = SystemVariablesUtils.GetKornPath()!;
+    static readonly string ListPath = Path.Combine(KornPath, "");
+
+    public static ModelJson Read()
+    {
+
+    }
+
+    public record ModelJson(
+        List<GithubPluginJson> OfficialPlugins,
+        List<LocalPluginJson> LocalPlugins
+    );
+
+    public record GithubPluginJson(string Name, string GithubRepository);
+    public record LocalPluginJson(string Name, string DirectoryPath, string ExecutableFileName);
+}
